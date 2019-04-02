042cbd9241823baf202e09bc87be792df3cd2b0d
ourazan <48337642+ourazan@users.noreply.github.com>
2019-04-02 08:43:33 -0500

    Add files via upload

---
+++ UnitTest/FileDataStore.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTest
+{
+    internal class FileDataStore
+    {
+        private string credPath;
+        private bool v;
+
+        public FileDataStore(string credPath, bool v)
+        {
+            this.credPath = credPath;
+            this.v = v;
+        }
+    }
+}
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoogleLoginService;
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ValidacionToken()
+        {
+           DatosUsuario   objeto = new DatosUsuario();
+           Assert.IsNotNull(  objeto.ValidarToken("4/HwHy9JvnqEgH-rig5sphjF72e-EV_H8nOy_9nIvB6ZsypEKKYqRbdyisG0A9ShbHNChl6j3vMki_PoDKUJbWUl0"
+                , "188187281435-7gksm80dotr93dvluh06gr8usa940ojp.apps.googleusercontent.com"
+                , "AamqXF5qNdQvNorgcHBI5Mfy"
+                , @"http://localhost:4771/DatosUsuario.aspx"));
+          
+        }
+    }
+}
